3f560fb9388affe376e7d6a0d9b7c33987e1c846
Caroline Aguiar <carolineclarissa19@gmail.com>
2021-05-23 22:35:18 -0300

    Continuação API agendamentos

---
+++ models/Agendamento.js
@@ -0,0 +1,57 @@
+const moment = require ('moment');
+const conexao = require('../infra/conexao');
+
+class Agendamento {
+
+    listagem(resp){
+        const sql = 'SELECT * FROM agendamentos';
+
+        conexao.query(sql, (error, results) => {
+            if(error) {
+                resp.status(400).json(error)
+            }
+            resp.status(201).json(results);
+        });
+    };
+
+    inserir(agendamento, resp) {
+        const sql = 'INSERT INTO agendamentos SET ?';
+
+        const data_servico = moment(agendamento.data_servico).format('YYYY-MM-DD');
+        const data_agendamento = moment().format('YYYY-MM-DD');
+
+        const agendamentoComData = {...agendamento, data_agendamento, data_servico};
+        const isDataValida = moment(agendamento.data_servico).isSameOrAfter(data_agendamento);
+        const isNomeCliente = agendamento.nome_cliente.length > 2;
+
+        const validaçoes = [
+            {
+                nome: "data_servico",
+                valido: isDataValida,
+                mensagem: "Data do agendamento deve ser igual ou posterior a data de hoje"
+            },
+            {
+                nome: "nome_cliente",
+                valido: isNomeCliente,
+                mensagem: "O nome do cliente deve ter ao menos 3 digitos"
+            }
+        ];
+
+        const errors = validaçoes.filter(campo => !campo.valido);
+
+        if(errors.length > 0) {
+            return resp.status(400).json(errors);
+        }
+
+        conexao.query(sql, agendamentoComData, (error, results) =>{
+            if(error){
+                throw error;
+            };
+
+            console.log(results);
+        });
+
+    };
+};
+
+module.exports = new  Agendamento;
