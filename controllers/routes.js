bc17dbe9dd305ce4f32ad984504caa8ff4ae2025
Caroline Aguiar <carolineclarissa19@gmail.com>
2021-05-24 19:13:08 -0300

    finalizando API

--- controllers/routes.js
+++ controllers/routes.js
@@ -5,10 +5,27 @@ module.exports = app => {
         Agendamento.listagem(resp);
     });
 
+    app.get('/agendamentos/:id', (req, resp) => {
+        const id = parseInt(req.params.id)
+        Agendamento.buscaPorId(id, resp);
+    });
+
     app.post('/agendamentos', (req, resp)=> {
         const agendamento = req.body;
 
         Agendamento.inserir(agendamento, resp);
+    });
 
+    app.put('/agendamentos/:id', (req, resp)=> {
+        const id = parseInt(req.params.id);
+        const agendamento = req.body;
+
+        Agendamento.alterar(id, agendamento, resp);
+    });
+
+    app.delete('/agendamentos/:id', (req, resp) => {
+        const id = parseInt(req.params.id);
+        Agendamento.remover(id, resp);
+        
     })
 };
